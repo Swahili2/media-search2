13cc99983d7ba95d9f2ff059eeb4368f0fc9f3c4
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-18 09:21:08 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -1,5 +1,5 @@
 from info import CHANNELS
-from utils import is_user_exist
+from utils import is_user_exist,add_user
 async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id if cmd.from_user else None
     if chat_id:
