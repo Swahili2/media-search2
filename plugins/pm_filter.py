7d22dcbd1b2a5bf1715695573d9a684b42417db1
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-18 09:07:08 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 from info import filters
-from status import handle_user_status
+from plugins.status import handle_user_status
 @Client.on_message(filters.text & filters.group & filters.incoming)
 async def group(client, message):
     await handle_user_status(client,message)
