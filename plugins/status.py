42778c528419e7459d8e577bfc126aecd90fdafb
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-19 13:03:23 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -11,3 +11,18 @@ async def handle_user_status(bot, cmd):
             )
         else:
             return
+async def handle_admin_status(bot, cmd):
+        
+        ban_status = await db.get_ban_status(chat_id)
+        if cmd.chat.type == "private":
+            return
+        if ban_status["is_banned"]:
+            if (
+                    datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+            ).days > ban_status["ban_duration"]:
+                await db.remove_ban(chat_id)
+        if await is_group_exist(cmd.chat.id):
+            await db.update_grd_id(chat_id,cmd.chat.id)
+        else:
+            await db.update_grd_id(chat_id,0)
+
