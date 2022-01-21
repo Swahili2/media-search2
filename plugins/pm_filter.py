b7e80dcb1da72121ac92bb6e19c036989fad74e5
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-21 08:16:41 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -8,12 +8,18 @@ from utils import get_filter_results
 async def group(client, message):
     await handle_user_status(client,message)
     await handle_admin_status(client,message)
+    group_status= await is_user_exist(message.chat.id)
+    if group_status:
+        for user in group_status:
+            user_id3 = user.group_id
+    else:
+        return
     if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
         return
     if 2 < len(message.text) < 50:    
         btn = []
         searchi = message.text
-        files = await get_filter_results(query=searchi)
+        files = await get_filter_results(searchi,user_id3)
         if files:
             await message.reply_text(f"<b>Bonyeza kitufe <b>(🔍Majibu ya Database : {len(files)})</b> Kisha subir kidogo,kisha chagua unachokipenda.\n\n💥Kwa urahisi zaidi kutafta chochote anza na aina kama ni  movie, series ,(audio ,video) kwa music , vichekesho kisha acha nafasi tuma jina la  kitu unachotaka mfano video jeje au audio jeje au movie extraction au series soz­</b>", reply_markup=get_reply_makup(searchi,len(files)))
         else:
