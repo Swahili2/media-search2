296f5a4595883f83a4e43f297c349c5050f293d6
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-21 08:18:09 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3,7 +3,7 @@ import re
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 from info import filters
 from plugins.status import handle_user_status,handle_admin_status
-from utils import get_filter_results
+from utils import get_filter_results,is_user_exist
 @Client.on_message(filters.text & filters.group & filters.incoming)
 async def group(client, message):
     await handle_user_status(client,message)
