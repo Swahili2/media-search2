e08852ad636386eb862329c7c24f51f49a27c3c0
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-22 09:52:21 +0300

    Create strings.py

---
+++ plugins/strings.py
@@ -0,0 +1,80 @@
+
+from pyrogram import __version__
+from info import (
+    OWNER_ID,
+    CUSTOM_START_MESSAGE
+)
+
+if CUSTOM_START_MESSAGE:
+    START_MESSAGE = CUSTOM_START_MESSAGE
+else:
+    START_MESSAGE = """<b>Hello {mention},
+I am an Inline Saver Bot, you can save inline filters and It can be use in any of your chats easily, Click help for more details</b> 
+"""
+
+HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
+○ <b>/add</b> <i>[keyword] [message or reply to message]</i>
+    <i>Add an Inline filter, you can use MarkDown for formatting</i>
+    
+○ <b>/delete</b> <i>[keyword]</i>
+    <i>Delete existing Filter</i>
+    
+○ <b>/filters</b>
+    <i>To see the filters</i>
+    
+○ <b>/stats</b>
+    <i>See the Bot's Statistics</i>
+    
+○ <b>/broadcast</b> <i>[reply to any message]</i>
+    <i>Broadcast any Messages to Bot users</i>
+    
+<b><u>Owner only Commands</u></b>
+○ <b>/delall</b>
+    <i>Delete all of the filters</i>
+    
+
+ABOUT_MESSAGE = f"""<b><u>ABOUT ME</u></b>
+<b>○ Maintained by : <a href='tg://user?id={OWNER_ID}'>This Person</a>
+○ Channel : <a href='https://t.me/CodeXBotz'>Code 𝕏 Botz</a>
+○ Support : <a href='https://t.me/CodeXBotzSupport'>Code 𝕏 Botz Support</a>
+○ Source Code : <a href='https://github.com/CodeXBotz/Inline-Filter-Bot'>Click here</a>
+○ Language : <a href='https://www.python.org/'>Python 3</a>
+○ Library : <a href='https://github.com/pyrogram/pyrogram'>Pyrogram Asyncio {__version__}</a></b>
+"""
+
+MARKDOWN_HELP = """<b><u>Markdown Formatting</u></b>
+○ <b>Bold Words</b> :
+    format: <code>*Bold Text*</code>
+    show as: <b>Bold Text</b>
+    
+○ <b>Italic Text</b>
+    format: <code>_Italic Text_</code>
+    show as: <i>Italic Text</i>
+    
+○ <b>Code Words</b>
+    format: <code>`Code Text`</code>
+    show as: <code>Code Text</code>
+    
+○ <b>Under Line</b>
+    format: <code>__UnderLine Text__</code>
+    show as: <u>UnderLine Text</u>
+    
+○ <b>StrikeThrough</b>
+    format: <code>~StrikeThrough Text~</code>
+    show as: <s>StrikeThrough Text</s>
+    
+○ <b>Hyper Link</b>
+    format: <code>[Text](https://t.me/CodeXBotz)</code>
+    show as: <a href='https://t.me/CodeXBotz'>Text</a>
+    
+○ <b>Buttons</b>
+    <u>Url Button</u>:
+    <code>[Button Text](buttonurl:https://t.me/CoddeXBotz)</code>
+    <u>Alert Button</u>:
+    <code>[Button Text](buttonalert:Alert Text)</code>
+    <u>In Sameline</u>:
+    <code>[Button Text](buttonurl:https://t.me/CodeXBotz:same)</code></i>
+○ <b>Notes:</b>
+    <i>Keep every Buttons in Seperate line when formating</i>
+    <i>Your alert message text must be less than 200 characters, otherwise bot will ignore that button</i>
+○ <b>Tip:</b> <i>You can add buttons for sticker and video note in /add command</i>"""
