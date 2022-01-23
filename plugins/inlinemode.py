5a56c1d44b49bd3fbf970204b6ef497de9d4bab9
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-23 07:47:17 +0300

    Update inlinemode.py

--- plugins/inlinemode.py
+++ plugins/inlinemode.py
@@ -125,7 +125,7 @@ async def give_filter(client: Client, query):
                     input_message_content=InputTextMessageContent(message_text = 'hii ni kwa sababu admin hajalipia kifurush'),
                     description='Tafadhal nchague Mimi nipo active mda wote',
                     thumb_url = user['title'],
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('👨‍👧‍👧 jiunge nami', url=f'tg://user?id={us_id]}')]])
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('👨‍👧‍👧 jiunge nami', url=f'tg://user?id={us_id}')]])
                 ))
         await query.answer(
             results = resultz,
