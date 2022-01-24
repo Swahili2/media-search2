2abb9f3cc02b38cd7d498113dd51a353fc38a5f3
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-24 13:15:34 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,10 +41,10 @@ class User(Document):
     group_id= fields.IntField(required=True)
     status = fields.StrField(required=True)
     title = fields.StrField(required=True)
-    link = fields.StrField(allow_none=True))
+    link = fields.StrField(allow_none=True)
     inv_link = fields.StrField(required=True)
     total_m = fields.StrField(required=True)
-    photo_id = fields.StrField(allow_none=True))
+    photo_id = fields.StrField(allow_none=True)
     class Meta:
         collection_name = COLLECTION_NAME_2
 
