a447c37f7abd99470eee4c7edd9661f672567533
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2022-02-06 07:59:18 +0300

    Update gdriveupdate.py

--- plugins/gdriveupdate.py
+++ plugins/gdriveupdate.py
@@ -1,196 +1,105 @@
 
-import re
 import os
 import pickle
-
-import urllib.parse as urlparse
-from urllib.parse import parse_qs
-
+import json
+from requests.utils import quote
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from config import DRIVE_NAME, DRIVE_ID, INDEX_URL
 
 
-def get_readable_file_size(size_in_bytes) -> str:
-    SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-    if size_in_bytes is None:
-        return '0B'
-    index = 0
-    while size_in_bytes >= 1024:
-        size_in_bytes /= 1024
-        index += 1
-    try:
-        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
-    except IndexError:
-        return 'File too large'
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
 
-class GoogleDriveSizeCalculate:
-    def __init__(self, service=None):
-        self.__G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-        self.__service = service
-        if service is None:
-            print("Pass service to GoogleDriveSizeCalculate class. Please fully read the instructions given in the repo.")
-            return
-        self.total_bytes = 0
-        self.total_files = 0
-        self.total_folders = 0
+class GoogleDriveHelper:
+    def __init__(self, name=None, listener=None):
+        self.__G_DRIVE_TOKEN_FILE = "token.pickle"
+        self.__OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive']
+        self.__service = self.authorize()
+        self.path = []
 
-    async def getIdFromUrl(link: str):
-        if "folders" in link or "file" in link:
-            regex = r"https://drive\.google\.com/(drive)?/?u?/?\d?/?(mobile)?/?(file)?(folders)?/?d?/([-\w]+)[?+]?/?(w+)?"
-            res = re.search(regex,link)
-            if res is None:
-                return "GDrive ID not found. Try sending url in different format."
-            return res.group(5)
-        parsed = urlparse.urlparse(link)
-        return parse_qs(parsed.query)['id'][0]
-
-    async def gdrive_checker(self, LINKorID):
-        if self.__service is None:
-            return
-        if 'drive.google.com' in LINKorID:
-            try:
-                file_id = self.getIdFromUrl(LINKorID)
-                if 'GDrive ID not found.' in file_id:
-                    print(file_id)
-                    return
-            except (KeyError, IndexError):
-                print("GDrive ID could not be found in the provided link.")
-                return
-        else:
-            file_id = LINKorID.strip()
-
-        error = False
-        print("Calculating... Please Wait!")
-
+    def get_readable_file_size(self, size_in_bytes) -> str:
+        if size_in_bytes is None:
+            return '0B'
+        index = 0
+        size_in_bytes = int(size_in_bytes)
+        while size_in_bytes >= 1024:
+            size_in_bytes /= 1024
+            index += 1
         try:
-            drive_file = self.__service.files().get(fileId=file_id, fields="id, name, mimeType, size",
-                                                   supportsTeamDrives=True,includeTeamDriveItems=True).execute()
-            name = drive_file['name']
-            if drive_file['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
-                typee = 'Folder'
-                self.total_folders += 1
-                self.gDrive_directory(**drive_file)
+            return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+        except IndexError:
+            return 'File too large'
+
+    def authorize(self):
+        # Get credentials
+        credentials = None
+        if os.path.exists(self.__G_DRIVE_TOKEN_FILE):
+            with open(self.__G_DRIVE_TOKEN_FILE, 'rb') as f:
+                credentials = pickle.load(f)
+        if credentials is None or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(Request())
             else:
-                try:
-                    typee = drive_file['mimeType']
-                except:
-                    typee = 'File'
-                self.total_files += 1
-                self.gDrive_file(**drive_file)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.__OAUTH_SCOPE)
+                credentials = flow.run_console(port=0)
 
-        except Exception as e:
-            print('\n')
-            if 'HttpError' in str(e):
-                h_e = str(e)
-                ori = h_e
-                try:
-                    h_e = h_e.replace('<', '').replace('>', '')
-                    h_e = h_e.split('when')
-                    f = h_e[0].strip()
-                    s = h_e[1].split('"')[1].split('"')[0].strip()
-                    e =  f"{f}\n{s}"
-                except:
-                    e = ori
-            print(str(e))
-            error = True
-        finally:
-            if error:
-                return
-            return {
-                'name': name,
-                'size': get_readable_file_size(self.total_bytes),
-                'bytes': self.total_bytes,
-                'type': typee,
-                'files': self.total_files,
-                'folders': self.total_folders
-                }
+            # Save the credentials for the next run
+            with open(self.__G_DRIVE_TOKEN_FILE, 'wb') as token:
+                pickle.dump(credentials, token)
+        return build('drive', 'v3', credentials=credentials, cache_discovery=False)
 
-    async list_drive_dir(self, file_id: str) -> list:
-        query = f"'{file_id}' in parents and (name contains '*')"
-        fields = 'nextPageToken, files(id, mimeType, size)'
-        page_token = None
-        page_size = 1000
-        files = []
-        while True:
-            response = self.__service.files().list(supportsTeamDrives=True,
-                                                  includeTeamDriveItems=True,
-                                                  q=query, spaces='drive',
-                                                  fields=fields, pageToken=page_token,
-                                                  pageSize=page_size, corpora='allDrives',
-                                                  orderBy='folder, name').execute()
-            files.extend(response.get('files', []))
-            page_token = response.get('nextPageToken', None)
-            if page_token is None:
-                break
-        return files
+    def drive_query(self, parent_id, fileName):
+        fileName = fileName.replace("'","\\'").replace('"','\\"')
+        gquery = " and ".join([f"name contains '{x}'" for x in fileName.split()])
+        query = f"'{parent_id}' in parents and ({gquery})"
+        response = self.__service.files().list(supportsTeamDrives=True,
+                                               includeTeamDriveItems=True,
+                                               q=query,
+                                               spaces='drive',
+                                               pageSize=200,
+                                               fields='files(id, name, mimeType, size)',
+                                               orderBy='modifiedTime desc').execute()["files"]
+        return response
 
-    async def gDrive_file(self, **kwargs):
-        try:
-            size = int(kwargs['size'])
-        except:
-            size = 0
-        self.total_bytes += size
+    def drive_list(self, fileName):
+        msg = ''
+        data = []
+        INDEX = -1
+        for parent_id in DRIVE_ID:
+            response = self.drive_query(parent_id, fileName)
+            INDEX += 1
+            for file in response:
+                if file['mimeType'] == "application/vnd.google-apps.folder":
+                    url_path = quote(f"{file['name']}")
+                    url = f'{INDEX_URL[INDEX]}/{url_path}/'
+                    data.append(
+                            {
+                                "type": "folder",
+                                "name": file['name'],
+                                "size": "none",
+                                "url": url,
+                                "drive_url": f"https://drive.google.com/drive/folders/{file['id']}"
+                                }
+                            )
+                else:
+                    size = self.get_readable_file_size(file.get('size'))
+                    url_path = quote(f'{file.get("name")}')
+                    url = f'{INDEX_URL[INDEX]}/{url_path}'
+                    data.append(
+                        {
+                            "type": "file",
+                            "name": file['name'],
+                            "size": size,
+                            "url": url,
+                            "drive_url": f"https://drive.google.com/uc?id={file['id']}&export=download"
+                            }
+                        )
+        # if len(data) == 0:
+        #     return {"error": "Found Literally Nothing"}
+        return data
 
-    async def gDrive_directory(self, **kwargs) -> None:
-        files = self.list_drive_dir(kwargs['id'])
-        if len(files) == 0:
-            return
-        for file_ in files:
-            if file_['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
-                self.total_folders += 1
-                self.gDrive_directory(**file_)
-            else:
-                self.total_files += 1
-                self.gDrive_file(**file_)
-
-#
-# ~ Notes ~
-# BUILD YOUR SERVICE HERE AND PASS TO THE GoogleDriveSizeCalculate CLASS
-# And remove """ quotes correspondingly
-#
-
-credentials = None
-oauth_scope = ['https://www.googleapis.com/auth/drive']
-
-"""
-# IF YOU USING token.pickle generated from credentials.json or yet to be generated:
-# ╾───────────────────────────────────────────────────────────────────────────────╼
-# Required credentials.json in this script's directory!
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as f:
-        credentials = pickle.load(f)
-if credentials is None or not credentials.valid:
-    if credentials and credentials.expired and credentials.refresh_token:
-        credentials.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', oauth_scope)
-        print(flow)
-        credentials = flow.run_console(port=0)
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(credentials, token)
-service =  build('drive', 'v3', credentials=credentials, cache_discovery=False)
-"""
-"""
-# IF YOU USING A SERVICE ACCOUNT IN NAME some_service_account.json
-# ╾──────────────────────────────────────────────────────────────╼
-# Required some_service_account.json file
-credentials = service_account.Credentials.from_service_account_file('some_service_account.json',scopes=oauth_scope)
-service =  build('drive', 'v3', credentials=credentials, cache_discovery=False)
-"""
-
-LINKorID = input("Paste your GoogleDrive file/folder's link/fileId : ")
-
-calculator = GoogleDriveSizeCalculate()  #(service)   ##Complete building service and then pass it
-calculate = calculator.gdrive_checker(LINKorID)
-
-
-# Note that, gdrive folder size calculating speed depends on how many files inside a folder.
-
-if not calculate is None:
-    print('\n')
-    for k, v in calculate.items():
-        print(f'{k.title()}:', v)
+drive = GoogleDriveHelper(None)
