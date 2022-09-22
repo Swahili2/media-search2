2f56dcd9cc852a7b04fecb47c18f620d53b81f58
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2022-09-22 17:07:18 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -256,7 +256,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             data1= details.msg2
             data2= data1.split(" ")[1]
             await client.send_message(chat_id = query.from_user.id,text=f"🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿\n{data1.split(" ")[0]}}\n {data1.split(" ")[2]}\n Tafadhali bonyeza kitufe hapo chini kuweza kulipia muda utakao weza kupata huduma hii"
-                    reply_markup=InlineKeyboardMarkup([replymkup2(f"week 1 tsh {data2.split(",")[0]}"),replymkup2(f"week 2 tsh {data2.split(",")[1]}"),replymkup2(f"week 3 tsh {data2.split(",")[2]}"),replymkup2(f"mwezi 1 tsh {data2.split(",")[3]}"),InlineKeyboardButton("back", callback_data=f"tanzania {fileid)
+                    reply_markup=InlineKeyboardMarkup([replymkup2(f"week 1 tsh {data2.split(",")[0]}"),replymkup2(f"week 2 tsh {data2.split(",")[1]}"),replymkup2(f"week 3 tsh {data2.split(",")[2]}"),replymkup2(f"mwezi 1 tsh {data2.split(",")[3]}"),[InlineKeyboardButton("back", callback_data=f"tanzania {fileid})])
                 )
         elif query.data.startswith("malipo"):
             await query.answer()
