1b9ae9c58798441590144dedcda81954ee3210d7
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2022-10-20 08:29:06 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,9 +1,6 @@
 from pyrogram import Client
-import re
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 from info import filters
-from plugins.status import handle_user_status,handle_admin_status
-from utils import get_filter_results,is_user_exist
-@Client.on_message(filters.text & filters.group & filters.incoming)
+@Client.on_message(filters.text & filters.private)
 async def group(client, message):
     
