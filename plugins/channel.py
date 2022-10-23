a5571c12f40872ebef83d5d416274b8c407c2fa6
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2022-10-23 07:59:47 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -9,6 +9,17 @@ async def group(client, message):
     if not status:
         return
     await client.send_message(chat_id= message.from_user.id,text="chagua huduma unayotaka kufanya marekebisho",
-            reply_markup =InlineKeyboardMarkup([[InlineKeyboardButton('No', callback_data = 'delallclose')],[InlineKeyboardButton('No', callback_data = 'delallclose')],[InlineKeyboardButton('No', callback_data = 'delallclose')],[InlineKeyboardButton('No', callback_data = 'delallclose')]])
+            reply_markup =InlineKeyboardMarkup([[InlineKeyboardButton('Rekebisha Makundi', callback_data = 'kundi')],[InlineKeyboardButton('Rekebisha Aina', callback_data = 'aina')],[InlineKeyboardButton('Rekebisha startup sms', callback_data = 'startup')],[InlineKeyboardButton('Rekebisha mawasiliano', callback_data = 'namba')]])
         )
     
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data == "kundi":
+        await query.answer()
+        mkv = await client.ask(text = " Samahani sana wateja wetu wa Kenya bado hatuja weka utaratibu mzuri.\n  hivi karibun tutaweka mfumo mzuri ili muweze kupata huduma zetu", chat_id = query.from_user.id)
+    elif query.data == "aina":
+        await query.answer()
+    elif query.data == "startup":
+        await query.answer()
+    elif query.data == "namba":
+        await query.answer()
