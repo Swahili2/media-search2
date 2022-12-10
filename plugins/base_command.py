04af380c6da40edf82465df03111da343042ad6b
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2022-12-10 10:45:49 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -250,7 +250,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.delete()
             mkv = await client.ask(text='⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️\nTafadhali tuma jina la kifurushi kipya bei ya wiki wiki2 wiki3 au mwezi kisha maelezo kidogo ya huduma hii zikitenganishwa na #@\n\n💫Mfano1 kifurushi cha vyote#@5000@6000#@7000#@8000#@Unaeza ukapata huduma zote ikiwemo series movies n.k\n\n💫Mfano2 kifurushi cha singo #@2000#@0#@0#@5000#@hapa utajipatia singo zilizotafsiriwa na ambazo hazijatafsiriwa tu \n\n💫Mfano3 Kifurushi cha tamthilia#@3500#@6000#@0#@8000#@hapa utajipatia tamthilia Kali ikiwemo huba\n\n⚡️Kumbuka ukiweka bei ni 0 hicho kipengele hakitakuepo kwenye kuonyesha bei za wiki za vifurush Vyako kwa wateja :💫mfano3 utaonyesha bei za wiki1,wik2,mwez. Ila wiki3 haitaonyesha',chat_id = query.from_user.id,reply_markup=ForceReply())
             try:
-                mkv1,mkv2,mkv3,mkv4,mkv5,mkv6=mkv.split("#@")
+                mkv1,mkv2,mkv3,mkv4,mkv5,mkv6=mkv.text.split("#@")
             except Exception as err:
                 await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
                 await mkv.delete()
