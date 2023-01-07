ef71aaab1171edcd0d2881d802fdcd9bcc0d061f
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-01-07 22:27:57 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -120,20 +120,20 @@ async def new_filtervip(client: Client, message):
             x+=1
             p.append(usr[gs].split('#@')[0])
             usrr=f'{usrr}\n{x}:{usr[gs].split("#@")[0]}'
-    mkv = await client.ask(text=f'CHAGUA KIFURUSHI YA KITU UNACHOTAKA KUHIFADHI \n (kwa kutuma namba ya kifurush husika kama itamilikiwa na zaid ya kifurushi kimoja tuna namba kifurushi kisha acha nafasi namba ya kifurushi kingine mfano 1 3)\n\n{usrr}',chat_id = message.from_user.id)
+    mkv = await client.ask(text=f'CHAGUA KIFURUSHI WAKILISHI YA KITU UNACHOTAKA KUHIFADHI \n (kwa kutuma namba ya kifurush husika kama itamilikiwa na zaid ya kifurushi kimoja tuna namba kifurushi kisha acha nafasi namba ya kifurushi kingine mfano 1 3 NOTE Media ina weza kumilikiwa na kifurushi 1 au viwili Tu sio zaidi)\n\n{usrr}',chat_id = message.from_user.id)
     try:
         try:
             ab5,ab6=mkv.text.spilt(" ",1)
             ab5= int(ab5)
             ab6 = int(ab6)
-            if i<ab5 or i<ab6:
+            if ab6>i<ab5 orab5==ab6 or ab5==0 or ab6==0:
                 await mkv.reply(text='tuma ujumbe sahihi kama ulivyo elekezwa idadi ya kutuma mwisho ni 6 anza upya')
                 return
             ab2 = f'g_{ab5} g_{ab6}'
         except:
             ab2 = int(mkv.text)
-            ab2 = f'g_{ab2}'
-            if i<ab2:
+            ab2 = f'g_{ab2} hrm45'
+            if i<ab2 and ab2!=0:
                 await mkv.reply(text='tuma ujumbe sahihi kama ulivyo elekezwa idadi ya kutuma mwisho ni 6 anza upya')
                 return
     except:
