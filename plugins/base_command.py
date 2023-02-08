9a8b797256ea093f9aeddbe4a5300cb6c597ddb6
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-02-08 18:03:45 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -348,7 +348,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             while a==False:
                 try:
                     mkv = await client.get_messages("me",(query.message.id)+1)
-                    a=True
+                    if mkv.text!=None:
+                        a=True
                 except:
                     a=False
             #mkv = await client.ask(text='⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️\nTafadhali Tuma maelezo kidogo kuhusu huduma/biashara unayo Fanya .Haya maelezo yataonekana endapo Mteja wako atakapo anza kumtumia robot huyu,\nKumbuka pia ukituma ujumbe wa zamani unafutwa kama ulishwahi tuma\n\nkwa maelezo zaidi mxheki @hrm45 akuelekeze zaidi\n\nukitaka kuadd jina andika {mention}.Mfano Mpendwa {mention}\n Karibu Swahili media',chat_id = query.from_user.id,reply_markup=ForceReply())
