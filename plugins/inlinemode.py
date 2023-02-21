4adc8eb2dfeadae6d9fc33bafbc77f8ce6624528
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-02-21 17:18:41 +0300

    Update inlinemode.py

--- plugins/inlinemode.py
+++ plugins/inlinemode.py
@@ -82,6 +82,8 @@ async def give_filter(client: Client, query):
                 try:
                     if status == True:
                         reply_markup=InlineKeyboardMarkup(eval(button)+[[InlineKeyboardButton(' Edit', url=f"https://t.me/{nyva}?start=subinps_-_-_-_e#{id3}")]])
+                    elif button ==  None:
+                        reply_markup = None
                     else:
                         reply_markup=InlineKeyboardMarkup(eval(button))
                     result = InlineQueryResultArticle(
