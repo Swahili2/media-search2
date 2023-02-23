b20b6994fa54261ffec3ea4e05999c1708958e3d
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-02-23 16:07:42 +0300

    Update inlinemode.py

--- plugins/inlinemode.py
+++ plugins/inlinemode.py
@@ -74,7 +74,7 @@ async def give_filter(client: Client, query):
         if button =="[]":
             reply_markup =None
         elif status == False:
-            relpy_markup =  InlineKeyboardMarkup(eval(button)+[[InlineKeyboardButton(' Chat Admin',user_id=int(group_id))]]))
+            relpy_markup =  InlineKeyboardMarkup(eval(button)+[[InlineKeyboardButton(' Chat Admin',user_id=int(group_id))]])
            
         elif status == True:
             reply_markup = InlineKeyboardMarkup(eval(button)+[[InlineKeyboardButton(' Edit', url=f"https://t.me/{nyva}?start=psbinps_-_-_-_{id3}")]])
