69fa7f4fdfd3b08e2e10ed20d33e14a1a9985d2b
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-04-14 13:40:40 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -154,7 +154,7 @@ async def start_msg_admins(client, message):
                         #await client.send_message(chat_id=int(group_id),f'hakikisha group zako zote kama umenruhusu kutengeneza invite link{ii}')        
                 await client.send_message(chat_id=cmd.from_user.id,text=f'Tafadhali Haupo kwenye database join kikundi kimoja wapo ili kupata huduma hii kisha tuma neno **hi** tujue kuwa wewe sio robot\n{group_d}')
                 return 
-            
+              
             if ban_status["is_banned"] == False and group_id != cmd.from_user.id :
                 await client.send_message(
                         chat_id=cmd.from_user.id,
