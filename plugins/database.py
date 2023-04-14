532a3eb7c8a6756a47d897192b3edfe7b2ee20f4
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-04-14 09:12:23 +0300

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -15,7 +15,7 @@ class Database:
             db_status=dict(
                 db_name = "SWAHILI GROUP MEDIA",
                 descp = "Tunahusika na uuzaji wa muvi na sizon kal zilizotafsiriwa kwa bei ",
-                phone_no = "0 hrm45 halopesa",
+                phone_no = "0 halopesa https://t.me/swahiliupdates hrm45",
                 ms_link= "link",
                 muda = "kuipakua mda wowote bila kikomo...",
                 g_1=  "hrm45",
