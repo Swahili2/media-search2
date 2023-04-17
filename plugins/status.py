63849c0229cfd73754e72278dc2fe5faa160b69d
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-04-17 07:10:22 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -1,7 +1,7 @@
 from info import CHANNELS
 import datetime
 from plugins.database import db
-from utils import is_user_exist,add_user,User
+from utils import is_user_exist,add_user,User,get_file_details
 async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id if cmd.from_user else None
     if chat_id:
@@ -39,4 +39,11 @@ async def handle_admin_status(bot, cmd):
         all_users =await db.get_all_acc()
         async for user in all_users:
             if user["ban_status"]["ban_duration"] < (datetime.date.today() - datetime.date.fromisoformat(user["ban_status"]["banned_on"])).days:
+                if user['file_id'].startswith(g_):
+                    abc=await db.get_db_status(user['db_name'])
+                    abc=f"{abc[user['file_id']].split('#@')[0]} kimeisha"
+            else:
+                for file in await get_file_details(user['file_id']):
+                    abc=f"{file.text.split('.dd#.')[0]} mda wake wa kuipakua umeisha"
+                await bot.send_message(chat_id=int(user['user_id'],text=f"{abc} tafadhali jiunge kuendelea kupata huduma zetu kwa bei nafuu'
                 await db.delete_acc(user['id'])
