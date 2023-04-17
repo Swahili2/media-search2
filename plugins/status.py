c7377f57b0df43e019c70aeb8c72b28f730db947
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-04-17 13:04:27 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -1,5 +1,5 @@
 from info import CHANNELS
-import datetime 
+from datetime import datetime 
 import time
 from plugins.database import db
 from utils import is_user_exist,add_user,User,get_file_details
@@ -34,13 +34,13 @@ async def handle_admin_status(bot, cmd):
             ban_status = await db.get_ban_status(user['id'])
             if ban_status["is_banned"]:
                 if ban_status["ban_duration"] < (
-                        datetime.datetime.now() - datetime.datetime.fromisoformat(ban_status["banned_on"])
+                        datetime.now() - datetime.fromisoformat(ban_status["banned_on"])
                 ).days:
                     await bot.send_message(chat_id=int(user['id']),text=f"Samahan admin kifurushi ulicho lipia kumtumia swahili robot kimeisha tafadhali lipia ili wateja wako waendelee kupata huduma zetu")
                     await db.remove_ban(user['id'])
         all_users =await db.get_all_acc()
         async for user in all_users:
-            if user["ban_status"]["ban_duration"] < (datetime.datetime.now() - datetime.datetime.fromisoformat(user["ban_status"]["banned_on"])).days:
+            if user["ban_status"]["ban_duration"] < (datetiime.now() - datetime.fromisoformat(user["ban_status"]["banned_on"])).days:
                 if user['file_id'].startswith(g_):
                     abc=await db.get_db_status(user['db_name'])
                     abc=f"{abc[user['file_id']].split('#@')[0]} kimeisha"
