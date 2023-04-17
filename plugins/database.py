a63016c44668120f05c070347c0884e389b645b9
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-04-17 12:15:56 +0300

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import time
 from info import DB2
 
@@ -12,7 +12,7 @@ class Database:
     def new_user(self, id):
         return dict(
             id=id,
-            join_date=datetime.datetime.now().isoformat(),
+            join_date=datetime.now().strftime("%m/%d/%Y  %H:%M:%S"),
             db_status=dict(
                 db_name = "SWAHILI GROUP MEDIA",
                 descp = "Tunahusika na uuzaji wa muvi na sizon kal zilizotafsiriwa kwa bei ",
@@ -29,7 +29,7 @@ class Database:
             ban_status=dict(
                 is_banned=False,
                 ban_duration=0,
-                banned_on=datetime.datetime.now().isoformat(),
+                banned_on=datetime.now().strftime("%m/%d/%Y  %H:%M:%S"),
                 ban_reason=''
             )
         )
@@ -41,7 +41,7 @@ class Database:
             db_name = db_name,
             ban_status=dict(
                 ban_duration=tme,
-                banned_on=datetime.datetime.now().isoformat(),
+                banned_on=datetime.now().strftime("%m/%d/%Y  %H:%M:%S"),
             )
         )
     async def add_acc(self, id,user_id,file_id,db_name,tme):
@@ -107,7 +107,7 @@ class Database:
         ban_status = dict(
             is_banned=True,
             ban_duration=ban_duration,
-            banned_on=datetime.date.today().isoformat(),
+            banned_on=datetime.now().strftime("%m/%d/%Y  %H:%M:%S"),
             ban_reason=ban_reason
         )
         await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status,'db_status.ms_link':link}})
