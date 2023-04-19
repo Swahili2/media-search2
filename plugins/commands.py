65acf868c18dda418759d43c1f6c39577cd89df2
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-04-19 08:29:31 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -638,7 +638,7 @@ async def del_filter(client: Client, message):
     found =await Media.count_documents(filter)
     if int(found) >=1:
         for dt in details:
-            for ad in await get_file_details(dt.text):
+            for ad in await get_file_details(dt.id):
                await Media.collection.delete_one({'text':ad.text})
         await Media.collection.delete_one(filter)
         await message.reply_text(
